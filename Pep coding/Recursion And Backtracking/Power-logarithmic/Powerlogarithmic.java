34f288d01b2ca8bfc490a75d94ebe4b0b3afd1b0
pushpdeepsingh <pushpdeepsinghv@gmail.com>
2022-04-27 18:10:25 +0530

    Recursion & Backtracking Power logarithmic pep coding

---
+++ Pep coding/Recursion And Backtracking/Power-logarithmic/Powerlogarithmic.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Powerlogarithmic {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here x raised to power n
+        //input
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        int ans = power(x,n);
+        System.out.println("the"+x+"raised to power"+n+"is"+ans);
+    }
+
+    public static int power(int x, int n){
+        if(n==1){
+            return x;
+        }
+        int ans = power(x,n-1)*x;
+        return ans;
+    }
+
+}
